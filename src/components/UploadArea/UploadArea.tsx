e335530658ea843bfbd846051c807cd216bbf9be
Vlad <rvlad2109@mail.ru>
2021-08-05 16:16:12 +0300

    refactor(File upload area): SAstudent-5 small code corrections
    refactor(File upload area): SAstudent-5 small code corrections

--- src/components/UploadArea/UploadArea.tsx
+++ src/components/UploadArea/UploadArea.tsx
@@ -1,9 +1,9 @@
 import React, { ChangeEvent, useRef, useState } from "react";
 import classNames from "classnames";
 import Loader from "./Loader/Loader";
-import { delay } from "./UploadFunction";
+import { delay } from "./uploadFunction";
 
-import classes from "./UploadArea.module.scss";
+import styles from "./UploadArea.module.scss";
 
 interface IUploadAreaProps {
   acceptTypes: string[];
@@ -27,22 +27,11 @@ export const UploadArea = ({
     accept: acceptTypes.join(","),
   };
 
-  // function typeCheck(elem: string) {
-  //   const types = acceptTypes
-  //   types.forEach((el) => {
-  //     return elem === el
-  //   })
-  //   //   return valid
-  //   // проверяем соответствует ли текущий элемент значению 30
-  // }
-
-  // acceptTypes.some(typeCheck)
-
   const typeCheck = (fileName: string) => {
     const types = acceptTypes;
     let valid = false;
     types.forEach((elem) => {
-      if (valid) return valid;
+      if (valid) return true;
       valid = fileName.includes(elem);
     });
     return valid;
@@ -106,21 +95,22 @@ export const UploadArea = ({
   const onDropHandler = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
 
-    let files = [...e.dataTransfer.files];
+    const files = [...e.dataTransfer.files];
+    const firstElem = files[0];
     if (
-      files[0].size < maxSizeOfFileInBytes &&
-      typeCheck(files[0].name) &&
-      files.length === 1
+      files.length === 1 &&
+      firstElem.size < maxSizeOfFileInBytes &&
+      typeCheck(firstElem.name)
     ) {
       setSize(false);
       setUploading(true);
-      setFileSize(Math.round(files[0].size / 1048576));
-      setFileName(files[0].name);
+      setFileSize(Math.round(firstElem.size / 1048576));
+      setFileName(firstElem.name);
 
       fetchFiles();
 
       const formData = new FormData();
-      formData.append("file", files[0]);
+      formData.append("file", firstElem);
     } else {
       setSize(true);
     }
@@ -131,68 +121,62 @@ export const UploadArea = ({
   return (
     <>
       {download ? (
-        <div className={classes.container}>
-          <div className={classes.file_info}>
+        <div className={styles.container}>
+          <div className={styles.file_info}>
             <div>
-              <p className={classes.file_name}>{fileName}</p>
-              <p className={classes.file_size}>{fileSize} Мб</p>
+              <p className={styles.file_name}>{fileName}</p>
+              <p className={styles.file_size}>{fileSize} Мб</p>
             </div>
-            <button
-              className={classes.deleteFile}
-              onClick={clearState}
-            ></button>
+            <button className={styles.deleteFile} onClick={clearState}></button>
           </div>
-          <div className={classes.status}>
-            <p className={classes.status_text}>Файл успешно загружен</p>
+          <div className={styles.status}>
+            <p className={styles.status_text}>Файл успешно загружен</p>
           </div>
         </div>
       ) : uploading ? (
-        <div className={classes.container}>
-          <div className={classes.uploading_file_info}>
-            <div className={classes.uploading_file_container}>
+        <div className={styles.container}>
+          <div className={styles.uploading_file_info}>
+            <div className={styles.uploading_file_container}>
               <Loader />
-              <div className={classes.uploading_file_container_div}>
-                <p className={classes.file_name}>{fileName}</p>
-                <p className={classes.file_size}>{fileSize} Мб</p>
+              <div className={styles.uploading_file_container_div}>
+                <p className={styles.file_name}>{fileName}</p>
+                <p className={styles.file_size}>{fileSize} Мб</p>
               </div>
             </div>
 
-            <button
-              className={classes.deleteFile}
-              onClick={clearState}
-            ></button>
+            <button className={styles.deleteFile} onClick={clearState}></button>
           </div>
-          <div className={classes.uploading_status}>
-            <p className={classes.status_text}>Файл загружается...</p>
+          <div className={styles.uploading_status}>
+            <p className={styles.status_text}>Файл загружается...</p>
           </div>
         </div>
       ) : (
         <div
           className={classNames(
-            size ? classes.sizeProblems : null,
-            onDrag ? classes.onDrag : null,
-            classes.UploadArea
+            size ? styles.sizeProblems : null,
+            onDrag ? styles.onDrag : null,
+            styles.UploadArea
           )}
           onDragStart={(e) => onDragStartHandler(e)}
           onDragLeave={(e) => onDragLeaveHandler(e)}
           onDragOver={(e) => onDragStartHandler(e)}
           onDrop={(e) => onDropHandler(e)}
         >
-          <p className={classes.text}>
+          <p className={styles.text}>
             Загрузите файл простым переносом или нажмите для выбора файла
           </p>
           <input
-            className={classes.input}
+            className={styles.input}
             type="file"
             ref={triggeredInput}
             onChange={changeHandler}
             {...options}
           ></input>
-          <button className={classes.btn} onClick={openTrigger}>
+          <button className={styles.btn} onClick={openTrigger}>
             Выбрать файл
           </button>
           {size ? (
-            <p className={classes.sizeWarning}>
+            <p className={styles.sizeWarning}>
               Размер файла не должен превышать{" "}
               {`${maxSizeOfFileInBytes / 1048576} `}
               Мб, а доступные расширения -{` ${options.accept}`}. Выберите
--- src/components/UploadArea/UploadFunction.ts
+++ src/components/UploadArea/UploadFunction.ts
@@ -18,20 +18,20 @@
 // UploadFunction().then(() => console.log('done'))
 
 export const delay = (ms: number) => {
-  return new Promise((r) => setTimeout(() => r(null), ms))
-}
+  return new Promise((r) => setTimeout(() => r(null), ms));
+};
 
-export let downloadResult = false
+export let downloadResult = false;
 
 export async function fetchFiles() {
-  console.log('Start uploading...')
+  console.log("Start uploading...");
 
-  await delay(4000)
-  const result = !downloadResult
-  console.log('done')
-  downloadResult = result
+  await delay(4000);
+  const result = !downloadResult;
+  console.log("done");
+  downloadResult = result;
 }
 
 export function uply() {
-  return true
+  return true;
 }
